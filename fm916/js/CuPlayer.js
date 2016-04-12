c7dd50bfe255319e49c71684e4cd55c6c647aead
CROTEL <crotel@users.noreply.github.com>
2016-04-12 11:46:03 +0800

    Update CuPlayer.js

--- fm916/js/CuPlayer.js
+++ fm916/js/CuPlayer.js
@@ -3,7 +3,7 @@
   var playlist = [{
       title:"正在直播 FM91.6",
       artist:"综合资讯广播",
-      m3u8:"http://aiseet.lszb.atianqi.com/app_4/yjgb1.m3u8?bitrate=2000",
+      mp3:"http://aiseet.lszb.atianqi.com/app_4/yjgb1.m3u8?bitrate=2000",
       poster: "/fm916/images/1.gif"
     }];
   
